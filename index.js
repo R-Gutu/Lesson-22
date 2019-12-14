719ad35692fbb68f3d9397d03c93403c872db62d
Roman <r.gutsu@bk.ru>
2019-12-14 13:57:41 +0200

    animation

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 var button = document.getElementById('cta');
 var topAnimation = document.querySelector('.top');
 var leftAnimation = document.querySelector('.left');
+var leftOneAnimation = document.querySelector('.leftone');
 button.addEventListener('click', anim);
 
 function anim(e){
 if (topAnimation.style.animationName === "" || topAnimation.style.animationName === "up"){
 topAnimation.style.animationName = 'down';
 leftAnimation.style.animationName = 'right';
+leftOneAnimation.style.animationName = 'leftone';
 button.innerHTML = 'Delete animation';
 } else {
     topAnimation.style.animationName = 'up';
     leftAnimation.style.animationName = 'left';
     button.innerHTML = 'Add animation';
+    rightAnimation.style.animationName = 'lefttwo';
 }
 
 }
